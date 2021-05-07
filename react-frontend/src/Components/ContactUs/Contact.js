36d5757da40d55202e0cbdd89afa93ae1a64e6ac
Rohit Kulshrestha <rohitkulshresth920@gmail.com>
2021-05-07 12:40:11 +0530

    Contact Form Functionality

--- react-frontend/src/Components/ContactUs/Contact.js
+++ react-frontend/src/Components/ContactUs/Contact.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import ContactPic from './ContactPic.jpg';
+import emailjs from 'emailjs-com';
 import {Row} from 'react-bootstrap';
 class Contact extends React.Component {
+
+    sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'template_kd7nuds', e.target, 'user_btDFJIdfNSInxkuAHWNaO')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      }
+
      render(){
     return (
-        <div style={{ width: "40%"}}>
+        <div style={{ width: "40%", border: "5px solid white", padding: "20px", borderRadius: "25px"}}>
+            <h1 style={{color: "white"}}>Contact US</h1>
+            <form onSubmit={this.sendEmail}>
             <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label" className="text-white font-weight-bold">Name</label>
-            <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Name"/>
+                <label for="exampleFormControlInput1" class="form-label" className="text-white font-weight-bold">Name</label>
+                <input type="name" class="form-control" id="exampleFormControlInput1" name="name" placeholder="Name"/>
+                <p style={{color: "white"}}>We will never share your name with anyone</p>
             </div>
             <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label" className="text-white font-weight-bold">Email</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email Address"/>
+                <label for="exampleFormControlInput1" class="form-label" className="text-white font-weight-bold">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" name="email" placeholder="Email Address"/>
+                <p style={{color: "white"}}>We will never share your email with anyone</p>
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label" className="text-white font-weight-bold">Query</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="3"></textarea>
             </div>
-            <button type="submit">Send Message</button>
-            
+            <input className="btn btn-primary" value="Send Message" type="submit"/>
+            </form>
         </div>
     );
      }
@@ -26,10 +44,10 @@ class Contact extends React.Component {
 export function ContactUsFinal()
 {
     return(
-        <div>
+        <div style={{height:"82vh"}}>
            <Row>
-           <Contact/>
-            <img src={ContactPic} style={{ height: "60vh", position: "relative", left: "43%", bottom: "330px",width:"54.6vw",border:"5px solid white"}}></img>
+           <Contact style={{border: "5px solid white"}}/>
+            <img src={ContactPic} style={{ height: "50vh", position: "relative", left: "43%", bottom: "450px",border:"5px solid white"}}></img>
            </Row>
         </div>
     );
