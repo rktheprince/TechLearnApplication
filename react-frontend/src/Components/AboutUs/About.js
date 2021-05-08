006ae1b142a932aa053224a7934891cad727cda2
Mohit <mohitmahi897968@gmail.com>
2021-05-08 15:16:11 +0530

    Update About.js

--- react-frontend/src/Components/AboutUs/About.js
+++ react-frontend/src/Components/AboutUs/About.js
@@ -19,7 +19,7 @@ class Author1 extends React.Component{
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title text-black">Taranjeet</h5>
-                        <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text text-black">Learning the art of programming, like most other disciplines, consists of first learning the rules and then learning when to break them.</p>
                         <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
@@ -41,7 +41,7 @@ class Author1 extends React.Component{
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title text-black">Mohit</h5>
-                        <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text text-black">And just like any company that blissfully ignored the Internet, the ones that dismiss the Internet of Things risk getting left behind.</p>
                         <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
@@ -62,7 +62,7 @@ class Author3 extends React.Component{
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title text-black">Soumik</h5>
-                    <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text text-black">PHP is about as exciting as your toothbrush. You use it every day, it does the job, it is a simple tool, so what? Who would want to read about toothbrushes?</p>
                     <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
@@ -84,7 +84,7 @@ class Author4 extends React.Component{
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title text-black">Piyush</h5>
-                        <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text text-black">The cloud services companies of all sizes…The cloud is for everyone. The cloud is a democracy.</p>
                         <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
@@ -106,7 +106,7 @@ class Author5 extends React.Component{
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title text-black">Rohit</h5>
-                        <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text text-black">Everyone knows that any scripting language shootout that doesn't show Python as the best language is faulty by design.</p>
                         <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
@@ -126,7 +126,7 @@ class Author6 extends React.Component{
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title text-black">Sayan</h5>
-                    <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text text-black">“We are entering a new world. The technologies of machine learning,Predicting the future isn’t magic, it’s artificial intelligence.”</p>
                     <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
@@ -148,7 +148,7 @@ class Author7 extends React.Component{
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title text-black">Divyansh</h5>
-                        <p class="card-text text-black">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text text-black">Information is not knowledge, and knowledge is not wisdom. Reading - even browsing - an old book can yield sustenance denied by a database search. Patience is a virtue, gluttony a sin.</p>
                         <p class="card-text text-black"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
